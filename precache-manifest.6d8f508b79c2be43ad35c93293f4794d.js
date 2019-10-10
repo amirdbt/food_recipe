2e955cecb90417772ee9baff21fec95ab9448db7
amirdbt <hassanamir643@yahoo.com>
2019-10-10 17:57:53 +0100

    Updates

---
+++ precache-manifest.6d8f508b79c2be43ad35c93293f4794d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8df284cbc2fde0589730f6de40c8aa2",
+    "url": "/food_recipe/index.html"
+  },
+  {
+    "revision": "f1fb6cd5b255e57b7393",
+    "url": "/food_recipe/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b16b0c601c2cb3f5aa98",
+    "url": "/food_recipe/static/css/main.5db7b5fb.chunk.css"
+  },
+  {
+    "revision": "f1fb6cd5b255e57b7393",
+    "url": "/food_recipe/static/js/2.de93687a.chunk.js"
+  },
+  {
+    "revision": "b16b0c601c2cb3f5aa98",
+    "url": "/food_recipe/static/js/main.cb756143.chunk.js"
+  },
+  {
+    "revision": "cc18024bb37cb71fc892",
+    "url": "/food_recipe/static/js/runtime-main.53d2f953.js"
+  }
+]);
